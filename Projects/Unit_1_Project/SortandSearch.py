33756df9625be7d558c733698c9944c453c46926
lainanguyen <lainanguyen7@gmail.com>
2023-02-11 23:55:11 -0500

    create unit 1 project with instructions

---
+++ Projects/Unit_1_Project/SortandSearch.py
@@ -0,0 +1,18 @@
+# 3 Sorting Methods
+# Paste into file as functions
+
+# 1 Linear Search function
+
+# 1 Binary Search function
+
+"""
+Output:
+a. How long does it take to perform on an array of 100 random integers?
+    - the 3 sorting methods
+    - binary search
+    - linear search
+for this one can use the time built in python fucntion
+
+b. Repeat for arrays of 1 000, 10 000, 100 000, 1 000 000
+
+"""
---
+++ Projects/Unit_1_Project/SortandSearchDisplay.py
@@ -0,0 +1,6 @@
+"""
+Use Matplotlib library
+Generate single scattersplot for eeach sort and srach method
+Should have five graphs
+Label each graph
+"""
